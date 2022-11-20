96224652bf25ec01f169eca04107c7ac0a59c02f
Jakob Simmons <JSimmons01778@gmail.com>
2022-11-20 14:53:11 -0500

    adj matrix

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-row = [0] * 100
-adj = [row] * 100
+adj = []
+for i in range(0,100):
+    adj.append([0] * 100)
 
 for i in range(0, 100):
     for j in range(i, 100):
         if j == i + 1 or j == i + 10:
             adj[i][j] = 1
             adj[j][i] = 1
-        else:
-            adj[i][j] = 0
 
 print(adj[0][1])
 for i in adj:
