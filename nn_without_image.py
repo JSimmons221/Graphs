83671e972174d411db3b95e1bb7596a15877eb61
Reese <rjhaly@wpi.edu>
2022-11-29 15:54:22 -0500

    Changed nn to non-image CNN

--- nn_without_image.py
+++ nn_without_image.py
@@ -7,8 +7,10 @@ import statsmodels.api as sm
 import plotly.express as px
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from tensorflow import keras
+from tensorflow.keras import layers
 from sklearn import metrics
+
 
 def train_model(adjmat):
     data = pd.read_csv("Graph.csv")
@@ -19,6 +21,8 @@ def train_model(adjmat):
         adjmatmat[i] = adjmat
 
     data = adjmatmat = pd.concat([data, pd.DataFrame(adjmatmat)], axis=1)
+    columns_to_normalize = ["start-x", "start-y", "end-x", "end-y", "query-x", "query-y"]
+    data[columns_to_normalize] = data[columns_to_normalize].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
     train_data, test_data = train_test_split(data, test_size=0.2, shuffle=True)
 
     X = train_data.drop(['shortest-path'], axis=1)
@@ -26,12 +30,15 @@ def train_model(adjmat):
 
     X_test = test_data.drop(['shortest-path'], axis=1)
     y_test = test_data[["shortest-path"]]
-    error_rate = []
-    for i in range(1,50):
-        print(i)
-        knn = KNeighborsClassifier(n_neighbors=i*4)
-        knn.fit(X, y)
-        y_pred = knn.predict(X_test)
-        y_pred = y_pred.reshape(len(y_pred),1)
-        error_rate.append(np.mean(y_pred != y_test))
-    print(error_rate)
+
+    model = keras.Sequential()
+    model.add(layers.Dense(500, activation='relu', input_dim=10006))
+    model.add(layers.Dense(100, activation='relu'))
+    model.add(layers.Dense(50, activation='relu'))
+    model.add(layers.Dense(2, activation='softmax'))
+
+    # Compile the model
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit(X, y, epochs=50, validation_data=(X_test, y_test))
