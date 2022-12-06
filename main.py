9453ef82f92b5ca4fc425bf812e74cd467f1ab9a
Reese <rjhaly@wpi.edu>
2022-12-06 17:14:47 -0500

    Working without image neural network!!

--- main.py
+++ main.py
@@ -4,26 +4,10 @@ import numpy as np
 import nn_without_image as nnwi
 import nn_with_image as nni
 import graphs as gp
+import more_graphs as mg
 
-adj = []
-for i in range(0,100):
-    adj.append([0] * 100)
-
-for i in range(0, 100):
-    for j in range(i, 100):
-        if j == i + 10:
-            adj[i][j] = 1
-            adj[j][i] = 1
-        if j == i + 1 and int(i/10) == int(j/10):
-            adj[i][j] = 1
-            adj[j][i] = 1
-
-adj_mat = np.array(adj)
-
-positions = {0: [0, 0]}
-for i in range(1, 100):
-    positions[i] = [i % 10, int((i - i % 10)/10)]
-
-nni.train_model(adj_mat)
-nnwi.train_model(adj_mat)
+n = 5
+mg.make_data(50000, r'Graph.csv', n)
+# nni.train_model(n)
+nnwi.train_model(n)
 
--- nn_without_image.py
+++ nn_without_image.py
@@ -1,6 +1,7 @@
 import statistics
 
 import math
+import ast
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
@@ -12,28 +13,45 @@ from tensorflow.keras import layers
 from sklearn import metrics
 
 
-def train_model(adjmat):
+def train_model(size):
     data = pd.read_csv("Graph.csv")
 
-    adjmat = adjmat.flatten()
-    adjmatmat = np.array([[0] * len(adjmat)] * len(data.index))
-    for i in range(len(data.index)):
-        adjmatmat[i] = adjmat
-
-    data = adjmatmat = pd.concat([data, pd.DataFrame(adjmatmat)], axis=1)
-    columns_to_normalize = ["start-x", "start-y", "end-x", "end-y", "query-x", "query-y"]
+    columns_to_normalize = ["sx","sy","mx","my","ex","ey"]
     data[columns_to_normalize] = data[columns_to_normalize].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+
+    newData = pd.DataFrame(index=range(len(data)), columns=range(len(data.columns) + 2 * size * (size - 1)))
+    newData.rename(columns={0: "adj_mat",
+                         1: "sx",
+                         2: "sy",
+                         3: "mx",
+                         4: "my",
+                         5: "ex",
+                         6: "ey",
+                         7: "on_path"}, errors='raise', inplace=True)
+    for i in range(len(data)):
+        adjmat = data.iloc[i]['adj_mat']
+        adjmat = ast.literal_eval(adjmat)
+        adjmat = pd.DataFrame(adjmat).T
+        cur_row = data.iloc[[i]]
+        new_row = pd.concat([cur_row.reset_index(drop=True), adjmat.reset_index(drop=True)], axis=1)
+        newData.iloc[[i]] = new_row
+    data = newData
+    data = data.drop(["adj_mat"], axis=1)
     train_data, test_data = train_test_split(data, test_size=0.2, shuffle=True)
 
-    X = train_data.drop(['shortest-path'], axis=1)
-    y = train_data[['shortest-path']]
+    X = train_data.drop(['on_path'], axis=1)
+    y = train_data[['on_path']]
+    X = np.asarray(X).astype(np.float32)
+    y = np.asarray(y).astype(np.float32)
 
-    X_test = test_data.drop(['shortest-path'], axis=1)
-    y_test = test_data[["shortest-path"]]
+    X_test = test_data.drop(['on_path'], axis=1)
+    y_test = test_data[['on_path']]
+    X_test = np.asarray(X_test).astype(np.float32)
+    y_test = np.asarray(y_test).astype(np.float32)
 
     model = keras.Sequential()
-    model.add(layers.Dense(10006*1, activation='relu', input_dim=10006))
-    model.add(layers.Dense(10006*1, activation='softmax'))
+    model.add(layers.Dense(256 * 1, activation='relu', input_dim=6 + 2 * size * (size - 1)))
+    model.add(layers.Dense(256*1, activation='softmax'))
 
     # Compile the model
     model.compile(optimizer='adam',
