a67d929610e7f85727e7ad487934cbad08df2f89
Reese <rjhaly@wpi.edu>
2022-12-12 14:31:20 -0500

    Testing with 25000 graphs

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ import numpy as np
 import nn_without_image as nnwi
 import nn_with_image as nni
 import graphs as gp
-import more_graphs as mg
+
 
 n = 5
-mg.make_data(25000, r'Graph.csv', 5)
-mg.csv_to_images(r'Graph.csv', 5)
 nnwi.train_model(n)
 nni.train_model(n)
 
